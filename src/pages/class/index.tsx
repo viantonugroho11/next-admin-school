273ab7e6f7eee889bd78dffd7d9aabfcf16debfc
Muhammad Novianto Nugroho <muhammad.nugroho@amartha.com>
2023-06-04 14:53:58 +0700

    first commit

---
+++ src/pages/class/index.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router'
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
+import Card from '@mui/material/Card'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import Button from '@mui/material/Button'
+
+// ** Demo Components Imports
+import TableClass from 'src/views/tables/TableClass'
+
+const MUITable = () => {
+  const router = useRouter()
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Typography variant='h5'>
+          <Link href='https://mui.com/components/tables/' target='_blank'>
+            MUI Tables
+          </Link>
+        </Typography>
+        <Button size='small' variant='contained' sx={{ marginBottom: 7 }} onClick={() => router.push('/class/create')}>
+          Create Class
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Class Table' titleTypographyProps={{ variant: 'h6' }} />
+          <TableClass />
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default MUITable
