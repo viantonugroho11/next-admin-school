7fac0c79d73e8640b5577d0dda56411b3fc7ddf4
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-06-14 02:59:12 +0700

    add env and auth action

---
+++ src/configs/keys.dev.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  URL_API: process.env.URL_API,
+  ROUTE_VERSION: process.env.ROUTE_VERSION,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  URL_APGATEWAY: process.env.URL_APGATEWAY,
+  FEATURE_FLAGS: process.env.FEATURE_FLAGS,
+  ID_TOKEN_KEY: process.env.ID_TOKEN_KEY,
+  SESSION_TOKEN_KEY: process.env.SESSION_TOKEN_KEY,
+  URL_API_MANAGEMENT: process.env.URL_API_MANAGEMENT
+}
