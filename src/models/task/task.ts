364ad14080586d0ff2ea0284d89f555acb99c333
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2023-08-12 21:02:07 +0700

    fixed indentasi task

--- src/models/task/task.ts
+++ src/models/task/task.ts
@@ -1,32 +1,32 @@
- export interface Task {
-    id: string
-    name: string
-    mapping_weekly_id: string
-    number_of_task: number
-    task_open_date: string
-    task_close_date: string
-    task_open_hour: string
-    task_close_hour: string
-    work_limit_task: number
-    status: number
-  }
+export interface Task {
+  id: string
+  name: string
+  mapping_weekly_id: string
+  number_of_task: number
+  task_open_date: string
+  task_close_date: string
+  task_open_hour: string
+  task_close_hour: string
+  work_limit_task: number
+  status: number
+}
 
-  export interface TaskResponse {
-    data: Task
-  }
+export interface TaskResponse {
+  data: Task
+}
 
-  export interface TasksResponse {
-    data: Task[]
-  }
+export interface TasksResponse {
+  data: Task[]
+}
 
-  export interface TaskParams {
-    name: string
-    mapping_weekly_id: string
-    number_of_task: number
-    task_open_date: string
-    task_close_date: string
-    task_open_hour: string
-    task_close_hour: string
-    work_limit_task: number
-    status: number
-  }
+export interface TaskParams {
+  name: string
+  mapping_weekly_id: string
+  number_of_task: number
+  task_open_date: string
+  task_close_date: string
+  task_open_hour: string
+  task_close_hour: string
+  work_limit_task: number
+  status: number
+}
